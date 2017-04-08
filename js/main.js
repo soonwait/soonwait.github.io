7be7f00179a7d8ec62c94a1c80236cb914d874eb
sun_wei <sun_wei@cdv.com>
2017-04-08 18:48:43 +0800

    添加代码行号

--- js/main.js
+++ js/main.js
@@ -51,4 +51,34 @@
     //     e.preventDefault()
     //     window.scrollTo(0,0)
     // })
+
+}());
+
+//////////////////////////代码前添加行号////////////////////////////
+(function() {
+    document.addEventListener("DOMContentLoaded", () => {
+        "use strict";
+        var elements = document.querySelectorAll('div.highlighter-rouge > pre.highlight');
+        Array.prototype.forEach.call(elements, function(pre, idx) {
+            var paras = pre.textContent.split("\n");
+            var lines = new Array(paras.length);
+            for(var i = 0; i < paras.length; i++) {
+                var wrap = Math.floor(paras[i].length / 70);
+                if (paras[i] == "" && i == paras.length - 1) {
+                    lines.splice(i, 1);
+                } else {
+                    lines[i] = i+1;
+                    for(var j = 0; j < wrap; j++) {
+                        lines[i] += "\n";
+                    }
+                }
+            }
+            var newCode = document.createElement("code");
+            newCode.innerHTML = lines.join("\n");
+            var newPre = document.createElement("pre");
+            newPre.className = "highlight lines";
+            newPre.appendChild(newCode);
+            pre.parentNode.insertBefore(newPre, pre);
+        });
+    });
 }());
